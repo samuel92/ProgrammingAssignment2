dd88f24a2c880ce623bb2ef2cad224b466ccc7fe
Samuel Carabeuf <samuel.carabeuf@yahoo.fr>
2014-07-27 22:27:40 +0200

    updated cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,39 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## This function, makeVector creates a special "matrix", which is really a list containing a function to
+## This function, makeCacheMatrix, creates a special "matrix", which is a list containing a function to
 ## set the matrix
 ## get the matrix
 ## set the inverse of the matrix
 ## get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-  MInv <- NULL
-  set <- function(y) {
-    x <<- y
-    MInv <<- NULL
-  }
-  get <- function() x
-  setinv <- function(inv) MInv <<- inv
-  getinv <- function() MInv
-  list(set = set, get = get,
-       setinv = setinv,
-       getinv = getinv)
+        MInv <- NULL
+        set <- function(y) {
+                x <<- y
+                MInv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) MInv <<- inv
+        getinv <- function() MInv
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Return a the cache matrix that is the inverse of 'x'
+## This function, CacheSolve, returns the "cache" matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
         
-  MInv <- x$getinv()
-  if(!is.null(MInv)) {
-    message("getting cached data")
-    return(MInv)
-  }
-  data <- x$get()
-  MInv <- solve(data, ...)
-  x$setinv(MInv)
-  MInv
+        MInv <- x$getinv()
+        if(!is.null(MInv)) {
+                message("getting cached data")
+                return(MInv)
+        }
+        data <- x$get()
+        if (dim(data)[1] == dim(data)[2]) {
+                MInv <- solve(data, ...)
+        }
+        x$setinv(MInv)
+        MInv
 }
 
